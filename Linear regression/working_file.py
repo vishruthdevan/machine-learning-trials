37bd57c288656d520459586a8fae5bf30a344546
Vishruth <vishruthdevan@gmail.com>
2022-01-20 19:51:23 +0530

    Data preprocessing

--- Linear regression/working_file.py
+++ Linear regression/working_file.py
@@ -6,3 +6,14 @@ from sklearn.utils import shuffle
 
 
 data = pd.read_csv("student-mat.csv", sep=";")
+
+data = data[['G1', 'G2', 'G3', 'studytime', 'failures', 'absences', 'Dalc']]
+
+predict = 'G3'
+
+
+X = np.array(data.drop(labels=[predict], axis=1))
+y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+    X, y, test_size=0.1)
