64b29a0deed752171a060e4eae07de8d26122b3f
Vishruth <vishruthdevan@gmail.com>
2022-01-23 15:55:47 +0530

    scatterplots for each feature and G3

--- Linear regression/working_file.py
+++ Linear regression/working_file.py
@@ -10,7 +10,7 @@ from matplotlib import style
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
-data = data[['G1', 'G2', 'G3', 'studytime', 'failures', 'absences', 'Dalc']]
+data = data[['G1', 'G2', 'G3', 'studytime', 'failures', 'absences']]
 
 predict = 'G3'
 
@@ -21,18 +21,18 @@ y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
     X, y, test_size=0.1)
 
-best_score = 0
-for _ in range(50):
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
-        X, y, test_size=0.1)
-    linear = linear_model.LinearRegression()
-    linear.fit(x_train, y_train)
-    accuracy = linear.score(x_test, y_test)
-    print(accuracy)
-    if accuracy > best_score:
-        best_score = accuracy
-        with open("student_linear_model.pickle", "wb") as f:
-            pickle.dump(linear, f)
+# best_score = 0
+# for _ in range(50):
+#     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+#         X, y, test_size=0.1)
+#     linear = linear_model.LinearRegression()
+#     linear.fit(x_train, y_train)
+#     accuracy = linear.score(x_test, y_test)
+#     print(accuracy)
+#     if accuracy > best_score:
+#         best_score = accuracy
+#         with open("student_linear_model.pickle", "wb") as f:
+#             pickle.dump(linear, f)
 
 
 linear_loaded = pickle.load(open("student_linear_model.pickle", "rb"))
@@ -45,3 +45,10 @@ predictions = linear_loaded.predict(x_test)
 
 for i in range(len(predictions)):
     print(predictions[i], x_test[i], y_test[i])
+
+p = 'absences'
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final grade G3")
+pyplot.show()
