9b6fcc609d43a9902050c09f46a28b7b6cee3de8
Vishruth <vishruthdevan@gmail.com>
2022-01-30 20:11:14 +0530

    Fit and score model

--- k means/working_file.py
+++ k means/working_file.py
@@ -1,3 +1,4 @@
+from mimetypes import init
 import numpy as np
 import sklearn
 from sklearn.preprocessing import scale
@@ -8,9 +9,9 @@ from sklearn import metrics
 digits = load_digits()
 data = scale(digits.data)
 
-x = digits.data
+x = data
 y = digits.target
-print(x, y)
+# print(x, y)
 k = len(np.unique(y))
 samples, features = data.shape
 
@@ -26,3 +27,7 @@ def bench_k_means(estimator, name, data):
              metrics.adjusted_mutual_info_score(y,  estimator.labels_),
              metrics.silhouette_score(data, estimator.labels_,
                                       metric='euclidean')))
+
+
+clf = KMeans(n_clusters=k, init='random', n_init=10, )
+bench_k_means(clf, "1", x)
